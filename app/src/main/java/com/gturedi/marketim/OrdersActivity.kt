2d1b8500a3e9a4a291937c5ce183326203ea0ec5
gturedi <turedi.gokhan@gmail.com>
2019-09-22 00:08:34 +0300

    added quit action

--- app/src/main/java/com/gturedi/marketim/OrdersActivity.kt
+++ app/src/main/java/com/gturedi/marketim/OrdersActivity.kt
@@ -1,8 +1,10 @@
 package com.gturedi.marketim
 
+import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_orders.*
 
 class OrdersActivity : BaseActivity() {
 
@@ -13,6 +15,18 @@ class OrdersActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_orders)
+
+        btnQuit.setOnClickListener {
+            AlertDialog.Builder(this)
+                .setTitle(R.string.quitBtn)
+                .setMessage(R.string.quitMessage)
+                .setNegativeButton(android.R.string.cancel, null)
+                .setPositiveButton(android.R.string.ok) { _, _ ->
+                    PreferencesService(this).rememberMe = false
+                    finish()
+                }
+                .show()
+        }
     }
 
 }
